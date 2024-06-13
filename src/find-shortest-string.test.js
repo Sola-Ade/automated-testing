2f34b4765569ddc5477a1feeff2597585e6025c7
Sola-Ade <solaadefarasin@hotmail.com>
2024-06-13 11:43:20 +0100

    try again.

--- src/find-shortest-string.test.js
+++ src/find-shortest-string.test.js
@@ -1,15 +1,13 @@
 // import function from local file
 const findShortestString = require("./find-shortest-string");
 
-
 test("findShortestString finds the shortest string in an array", () => {
-    expect(findShortestString(["it", "is", "a", "nice", "day"])).toBe("a");
-    expect(findShortestString(["why", "hello", "to", "you"])).toBe("to");
-  });
-  
-  test("findShortestString returns the last string in cases of joint Shortest strings", () => {
-    expect(findShortestString(["brave", "dance"])).toBe("dance");
-    expect(findShortestString(["funny", "bravo"])).toBe("bravo");
-    expect(findShortestString(["crazy", "observe"])).toBe("observe");
-  });
-  
+  expect(findShortestString(["it", "is", "a", "nice", "day"])).toBe("a");
+  expect(findShortestString(["why", "hello", "to", "you"])).toBe("to");
+});
+
+test("findShortestString returns the last string in cases of joint Shortest strings", () => {
+  expect(findShortestString(["brave", "dance"])).toBe("dance");
+  expect(findShortestString(["funny", "bravo"])).toBe("bravo");
+  expect(findShortestString(["crazy", "observe"])).toBe("observe");
+});
