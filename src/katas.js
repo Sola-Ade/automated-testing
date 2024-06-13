421e0c27f9065c77a0ef6e533a8045f579fcbb4f
Sola-Ade <solaadefarasin@hotmail.com>
2024-06-13 13:37:44 +0100

    Add jest test cases.

--- src/katas.js
+++ src/katas.js
@@ -1,3 +1,9 @@
+/* Push the source array to the end of the destination array.
+ *
+ * @param {sourceArr[]} sourceArr - array of all
+ * @param {destArr[]} destArr -  array of all
+ * @returns {destArr} - the new string with sourceArr attached to the end of destArr*/
+
 function copyArrayContent(sourceArr, destArr) {
   for (let element of sourceArr) {
     destArr.push(element);
--- src/katas.test.js
+++ src/katas.test.js
@@ -1,19 +1,11 @@
 // import function from local file
 const copyArrayContent = require("./katas");
 
-console.log(copyArrayContent(["cat", "dog", "fox"], ["bee", "turtle"])),
-  "expect"[("bee", "turtle", "cat", "dog", "fox")];
-
-console.log(copyArrayContent([8, 10, 11], [5, 7])), "expect"[(5, 7, 8, 10, 11)];
-
-console.log(copyArrayContent(["oranges", "cat", "lamp"], [5, 2])),
-  "expect"[(5, 2, "oranges", "cat", "lamp")];
-
-console.log(copyArrayContent([5, 2], ["oranges", "cat", "lamp"])),
-  "expect"[("oranges", "cat", "lamp", 5, 2)];
-
-console.log(copyArrayContent([-15, -5], [-11, -3])),
-  "expect"[(-11, -3, -15, -5)];
-
-console.log(copyArrayContent([5.1, 1.5, 5.2, 6.3], [3.2, 5.6, 9.8])),
-  "expect"[(3.2, 5.6, 9.8, 5.1, 1.5, 5.2, 6.3)];
+test("copyArrayContent pushes the source array to the end of the destination array", () => {
+  expect(copyArrayContent(["cat", "dog", "fox"], ["bee", "turtle"])).toBe[
+    ("bee", "turtle", "cat", "dog", "fox")
+  ];
+  expect(copyArrayContent(["oranges", "cat", "lamp"], [5, 2])).toBe[
+    (5, 2, "oranges", "cat", "lamp")
+  ];
+});
